ef898f3577824c7f937924a615f33e3f3f98a4c4
Bollemii <100069388+Bollemii@users.noreply.github.com>
2024-04-11 01:32:29 +0200

    add battery state detection

--- escape-game/src/screens/piratesdelilebourbon/FirstStep.tsx
+++ escape-game/src/screens/piratesdelilebourbon/FirstStep.tsx
@@ -1,28 +1,54 @@
-import { useState } from "react";
-import { Image, StyleSheet, View } from "react-native"
+import { useEffect, useState } from "react";
+import { Image, StyleSheet, Text, View } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { addBatteryStateListener } from "expo-battery";
+import { Camera, PermissionStatus, FlashMode } from "expo-camera";
 
+import { constants } from "../../constants";
 import BackButton from "../../components/BackButton";
-import SpeechPanel from "../../components/speechPanel";
+import SpeechPanel from "../../components/SpeechPanel";
 import { getSavedPseudo } from "../../dataaccess/playerData";
 
 const data = {
     dark: {
-        image: require("../../../assets/images/calle-sombre.png"),
+        image: require("../../../assets/images/piratesdelilebourbon/dark-ships-hold.png"),
         text: "Où suis-je ? La noirceur m’enveloppe, et les tonneaux de rhum semblent se moquer de moi. Mais attendez… des voix étouffées viennent du pont. Je devrais absolument aller voir s’il y a des gens là-haut. Le problème, c’est qu’on y voit pas grand chose ici."
     },
     light: {
-        image: require("../../../assets/images/calle-clair.png"),
+        image: require("../../../assets/images/piratesdelilebourbon/light-ships-hold.png"),
         text: "Ok, voilà qui est mieux ! Avec ma lampe allumée, je peux enfin voir où je mets les pieds. Maintenant, il est temps de sortir de cette pièce. Pas question de rester coincé ici plus longtemps."
     }
 }
 
 export default function FirstStep () {
+    const navigation = useNavigation()
     const [pseudo, setPseudo] = useState("")
-    const [lightOn, setLightOn] = useState(false)
     getSavedPseudo().then((pseudo) => setPseudo(pseudo || "Joueur"))
+    const [lightOn, setLightOn] = useState(false)
+
+    const batteryListener = addBatteryStateListener(({ batteryState }) => {
+        const isCharging = batteryState === 2
+        if (isCharging !== lightOn) {
+            setLightOn(batteryState === 2)
+        }
+    })
+
+    useEffect(() => {
+        return () => {            
+            batteryListener.remove()
+        }
+    }, [])
+
+    const win = () => {
+        // @TODO Save the state of the game
+
+        // @ts-expect-error: navigation type is not well defined
+        navigation.navigate(constants.screens.game[2])
+    }
 
     return (
         <View>
+            <Camera flashMode={lightOn ? FlashMode.torch : FlashMode.off}/>
             <Image
                 source={lightOn ? data.light.image : data.dark.image}
                 style={styles.image}
@@ -37,6 +63,11 @@ export default function FirstStep () {
 }
 
 const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
     image: {
         width: '100%',
         height: '100%',
