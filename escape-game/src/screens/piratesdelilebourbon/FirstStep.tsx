c67318c6f830196effbaedaabf84ada935b22abf
Bollemii <100069388+Bollemii@users.noreply.github.com>
2024-04-10 14:09:48 +0200

    set image and text in data constant

--- escape-game/src/screens/piratesdelilebourbon/FirstStep.tsx
+++ escape-game/src/screens/piratesdelilebourbon/FirstStep.tsx
@@ -5,6 +5,17 @@ import BackButton from "../../components/BackButton";
 import SpeechPanel from "../../components/speechPanel";
 import { getSavedPseudo } from "../../dataaccess/playerData";
 
+const data = {
+    dark: {
+        image: require("../../../assets/images/calle-sombre.png"),
+        text: "Où suis-je ? La noirceur m’enveloppe, et les tonneaux de rhum semblent se moquer de moi. Mais attendez… des voix étouffées viennent du pont. Je devrais absolument aller voir s’il y a des gens là-haut. Le problème, c’est qu’on y voit pas grand chose ici."
+    },
+    light: {
+        image: require("../../../assets/images/calle-clair.png"),
+        text: "Ok, voilà qui est mieux ! Avec ma lampe allumée, je peux enfin voir où je mets les pieds. Maintenant, il est temps de sortir de cette pièce. Pas question de rester coincé ici plus longtemps."
+    }
+}
+
 export default function FirstStep () {
     const [pseudo, setPseudo] = useState("")
     const [lightOn, setLightOn] = useState(false)
@@ -13,19 +24,13 @@ export default function FirstStep () {
     return (
         <View>
             <Image
-                source={lightOn
-                    ? require("../../../assets/images/calle-clair.png")
-                    : require("../../../assets/images/calle-sombre.png")
-                }
+                source={lightOn ? data.light.image : data.dark.image}
                 style={styles.image}
             />
             <BackButton text="Quitter" pageRedirect="Home"/>
             <SpeechPanel 
                 speaker={pseudo}
-                text={lightOn
-                    ? "Ok, voilà qui est mieux ! Avec ma lampe allumée, je peux enfin voir où je mets les pieds. Maintenant, il est temps de sortir de cette pièce. Pas question de rester coincé ici plus longtemps."
-                    : "Où suis-je ? La noirceur m’enveloppe, et les tonneaux de rhum semblent se moquer de moi. Mais attendez… des voix étouffées viennent du pont. Je devrais absolument aller voir s’il y a des gens là-haut. Le problème, c’est qu’on y voit pas grand chose ici."
-                }
+                text={lightOn ? data.light.text : data.dark.text}
             />
         </View>
     );
--- escape-game/src/screens/piratesdelilebourbon/Introduction.tsx
+++ escape-game/src/screens/piratesdelilebourbon/Introduction.tsx
@@ -5,6 +5,16 @@ import { constants } from "../../constants";
 import BackButton from "../../components/BackButton";
 import Button from "../../components/Button";
 
+const data = {
+    text : [
+        "Bienvenue à bord, moussaillon !",
+        "Vous voilà plongé dans l’univers impitoyable des pirates, à bord du navire “Le Victorieux”.",
+        "Votre mission ? Survivre, résoudre des énigmes et échapper à ce monde de corsaires et de trésors maudits.",
+        "Prêt à lever l'ancre et à vous aventurer dans l'inconnu ?",
+        "Que votre esprit d'aventure vous guide vers de nouveaux horizons !",
+        "Avant de commencer votre périple à travers l'univers des pirates, veuillez noter que cette aventure nécessite l'utilisation des fonctionnalités spécifiques de votre téléphone."
+    ]
+}
 
 export default function Introduction() {
     const navigation = useNavigation();
@@ -19,12 +29,12 @@ export default function Introduction() {
             <BackButton text="Quitter" pageRedirect="Home"/>
             <Text style={styles.title}>Pirates de l'Ile Bourbon</Text>
             <View style={{marginHorizontal: 20}}>
-                <Text style={[styles.text, {fontWeight: 'bold', marginBottom: 10}]}>Bienvenue à bord, moussaillon !</Text>
-                <Text style={styles.text}>Vous voilà plongé dans l’univers impitoyable des pirates, à bord du navire “Le Victorieux”.</Text>
-                <Text style={styles.text}>Votre mission ? Survivre, résoudre des énigmes et échapper à ce monde de corsaires et de trésors maudits.</Text>
-                <Text style={styles.text}>Prêt à lever l'ancre et à vous aventurer dans l'inconnu ?</Text>
-                <Text style={styles.text}>Que votre esprit d'aventure vous guide vers de nouveaux horizons !</Text>
-                <Text style={[styles.text, {marginTop: 50}]}>Avant de commencer votre périple à travers l'univers des pirates, veuillez noter que cette aventure nécessite l'utilisation des fonctionnalités spécifiques de votre téléphone.</Text>
+                <Text style={[styles.text, {fontWeight: 'bold', marginBottom: 10}]}>{data.text[0]}</Text>
+                <Text style={styles.text}>{data.text[1]}</Text>
+                <Text style={styles.text}>{data.text[2]}</Text>
+                <Text style={styles.text}>{data.text[3]}</Text>
+                <Text style={styles.text}>{data.text[4]}</Text>
+                <Text style={[styles.text, {marginTop: 50}]}>{data.text[5]}</Text>
             </View>
             <Button text="Commencer" onPress={handlePress} buttonStyle={styles.button}/>
         </View>
